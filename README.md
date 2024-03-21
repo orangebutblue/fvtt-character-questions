08bbc381e12903b22c614f6c6642c6b0dfdcb104
orangebutblue <letsdropthehammer@gmail.com>
2024-03-21 11:09:35 +0100

    v0.1.1 * Question button is now only visible for GM * updated README.md

--- README.md
+++ README.md
@@ -10,10 +10,37 @@ The module posts a random question to chat based on the different selected categ
 
 `https://raw.githubusercontent.com/orangebutblue/fvtt-character-questions/main/module.json`
 
+## Usage
+As a GM, just select the question mark icon from the token control bar
+
+![Token Control Bar](https://imgur.com/yUOvkXe.png)
+
+Then select the categories you want questions from (I recommend only using a single category at a time) and the number of questions you want to fetch at the same time.
+
+All questions will be whispered to the GM. If you like a question, right-click it and select 'Reveal to Everyone' (or just ask them the question yourself).
+
+![](https://imgur.com/u0vD0fn.png)
+
 ## Contributing
+
+### Bugs and feature requests
+This is my first Foundry module, so I'm sure there are a lot of things that can be improved. If you find a bug or have a feature request, please open an [issue](https://github.com/orangebutblue/fvtt-character-questions/issues) here on GitHub
 
 ### New questions
 Contributions to the Character Questions are welcome! If you want to add your own questions to the module, just add it to the questions.json and send me a merge request. You can add the question in any language you like, however it is mandatory for every question to have at least an English translation.
 
+## Requirements for new questions:
+- Questions should be formulated as if addressing the character directly.
+  - Example: "What is your favorite color?" instead of "What is the character's favorite color?"
+- Questions should be open-ended and not have a yes/no answer
+- Questions should be categorized accordingly. If you want a new category to be added, open an issue and I will consider it.
+- Questions should be written in a way that they can be answered by any character, regardless of the setting or system.
+  - Example: "What is your favorite spell?" is not suitable, as it assumes the character is a spellcaster.
+- Before adding a new question, please check if there isn't already a similar question in the list.
 ### Translating
-Character Questions can support multiple languages. If you want, you can translate one of the existing questions to the language of your choosing and send me a merge request
+Character Questions can support multiple languages. If you want, you can translate the existing questions to the language of your choice.
+Just add another key to an existing question with your language code and the translation.
+Example:
+```json
+{"en": "What is your favorite color?","es": "¿Cuál es tu color favorito?"}
+```
--- scripts/character-questions.js
+++ scripts/character-questions.js
@@ -12,9 +12,6 @@ class CharacterQuestions extends FormApplication {
         // Retrieve the saved form data
         const formData = game.settings.get('character-questions', 'formData');
 
-        // Log the retrieved data
-        console.log('Retrieved form data:', formData);
-
         // Set the selected state for each form field
         Object.keys(formData).forEach(key => {
             formData[key + 'Selected'] = formData[key];
@@ -44,9 +41,6 @@ class CharacterQuestions extends FormApplication {
             formObject[element.name] = element.checked;
         });
 
-        // Log the form data to be saved
-        console.log('Form data to be saved:', formObject);
-
         // Save the form data
         game.settings.set('character-questions', 'formData', formObject);
 
@@ -71,7 +65,7 @@ class CharacterQuestions extends FormApplication {
                 if (data[category]) {
                     const items = data[category];
                     const gmIds = game.users.filter(user => user.isGM).map(gm => gm.id);
-                    if(numQuestions > items.length){
+                    if (numQuestions > items.length) {
                         ChatMessage.create({
                             user: game.user._id,
                             whisper: gmIds,
@@ -80,14 +74,14 @@ class CharacterQuestions extends FormApplication {
                     }
                     for (let i = 0; i < numQuestions; i++) {
                         const randomIndex = Math.floor(Math.random() * items.length);
+                        // return english text if selectedLanguage not available
                         const randomItem = items[randomIndex][selectedLanguage] || items[randomIndex].en;
-
-                        // Remove the selected item from the array to prevent duplicates
+                        // Remove selection to prevent duplicate picks
                         items.splice(randomIndex, 1);
                         ChatMessage.create({
                             user: game.user._id,
                             whisper: gmIds,
-                            content: `Question regarding your <i>${category}</i>:<br/><b>${randomItem}</b>`,
+                            content: `Question about <i>${category}</i>:<br/><b>${randomItem}</b>`,
                         });
                     }
                 }
@@ -100,33 +94,34 @@ class CharacterQuestions extends FormApplication {
 
 
 Hooks.on("getSceneControlButtons", (controls) => {
-    const newButton = {
-        name: "character-question-button",
-        title: "Character Questions",
-        icon: "fa-solid fa-question",
-        onClick: () => {
-            new CharacterQuestions().render(true);
-        },
-        button: true
-    };
+    if (game.user.isGM) {
+        const newButton = {
+            name: "character-question-button",
+            title: "Character Questions",
+            icon: "fa-solid fa-question",
+            onClick: () => {
+                new CharacterQuestions().render(true);
+            },
+            button: true
+        };
 
-    // place where to add the button to
-    const tokenControls = controls.find((control) => control.name === "token");
-    if (tokenControls) {
-        tokenControls.tools.push(newButton);
+        const tokenControls = controls.find((control) => control.name === "token");
+        if (tokenControls) {
+            tokenControls.tools.push(newButton);
+        }
     }
 });
-Hooks.once('init', function() {
+Hooks.once('init', function () {
     game.settings.register('character-questions', 'formData', {
         name: 'Form Data',
         hint: 'Data from the form',
-        scope: 'world', // This specifies a world-level setting
-        config: false, // This specifies that the setting does not appear in the settings menu
-        default: {}, // Default value is an empty object
+        scope: 'world', // specifies a world-level setting
+        config: false, // specifies that the setting does not appear in the settings menu
+        default: {},
         type: Object,
     });
     // Register Handlebars helper
-    Handlebars.registerHelper('selected', function(value, expectedValue) {
+    Handlebars.registerHelper('selected', function (value, expectedValue) {
         return value === expectedValue ? 'selected' : '';
     });
 });
